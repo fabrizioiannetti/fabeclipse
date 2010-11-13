1c8d54941e9397152e3d07e4454672ed134e1844
Fabrizio Iannetti <fabrizio.iannetti@gmail.com>
2010-11-13 15:43:27 +0100

    * sync position of text editor with position in grep view  * use ruler to show original line numbers in grep view  * grep view is read only   * highlight cursor line in grep view

--- com.github.fabeclipse.textedgrep/src/com/github/fabeclipse/textedgrep/GrepTool.java
+++ com.github.fabeclipse.textedgrep/src/com/github/fabeclipse/textedgrep/GrepTool.java
@@ -15,6 +15,37 @@ import org.eclipse.ui.texteditor.AbstractTextEditor;
 
 public class GrepTool {
 
+	public static class GrepContext {
+		private int[] lineMap;
+		private final IDocument document;
+		private final StringBuilder grep;
+		private final int numGrepLines;
+		public GrepContext(IDocument document, StringBuilder grep, int[] lineMap, int numGrepLines) {
+			this.document = document;
+			this.grep = grep;
+			this.lineMap = lineMap;
+			this.numGrepLines = numGrepLines;
+		}
+		
+		public int getOriginalLine(int grepLine) {
+			int originalLine = lineMap[grepLine];
+			return originalLine;
+		}
+		
+		public String getText() {
+			return grep.toString();
+		}
+		
+		public IDocument getDocument() {
+			return document;
+		}
+		
+		public int getMaxOriginalLine() {
+			return lineMap[numGrepLines - 1];
+		}
+		
+	}
+
 	private String regex;
 
 	private static IDocumentListener listener = new IDocumentListener() {
@@ -34,7 +65,10 @@ public class GrepTool {
 		this.regex = regex;
 	}
 
-	public String grepCurrentEditor(IWorkbenchWindow window) {
+	public GrepContext grepCurrentEditor(IWorkbenchWindow window) {
+		GrepContext grepContext = null;
+		// start with 10 thousands lines (in the grep result)
+		int[] lineMap = new int[10000];
 		StringBuilder grep = new StringBuilder();
 		IEditorPart activeEditor = window.getActivePage().getActiveEditor();
 		if (activeEditor instanceof AbstractTextEditor) {
@@ -48,14 +82,25 @@ public class GrepTool {
 			Matcher matcher = Pattern.compile(regex).matcher("");
 			Formatter formatter = new Formatter(grep);
 			int lineNum = 0;
+			int grepLineNum = 0;
 			while(s.hasNextLine()) {
 				String line = s.nextLine();
 				if (matcher.reset(line).find()) {
-					formatter.format("%5d %s\n", lineNum, line);
+					formatter.format("%s\n", line);
+					if (grepLineNum >= lineMap.length) {
+						// resize lineMap adding 10 thousand elements
+						int[] newLineMap = new int[lineMap.length + 10000];
+						System.arraycopy(lineMap, 0, newLineMap, 0, lineMap.length);
+						lineMap = newLineMap;
+					}
+					lineMap[grepLineNum++] = lineNum;
 				}
 				lineNum++;
 			}
+			// remove trailing newline
+			grep.deleteCharAt(grep.length()-1);
+			grepContext = new GrepContext(document, grep, lineMap, grepLineNum);
 		}
-		return grep.toString();
+		return grepContext;
 	}
 }
--- com.github.fabeclipse.textedgrep/src/com/github/fabeclipse/textedgrep/views/GrepView.java
+++ com.github.fabeclipse.textedgrep/src/com/github/fabeclipse/textedgrep/views/GrepView.java
@@ -3,22 +3,36 @@ package com.github.fabeclipse.textedgrep.views;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.CursorLinePainter;
 import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.TextViewer;
+import org.eclipse.jface.text.source.CompositeRuler;
+import org.eclipse.jface.text.source.IVerticalRuler;
+import org.eclipse.jface.text.source.LineNumberRulerColumn;
+import org.eclipse.jface.text.source.SourceViewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CaretEvent;
+import org.eclipse.swt.custom.CaretListener;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IMemento;
 import org.eclipse.ui.IViewSite;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.part.ViewPart;
+import org.eclipse.ui.texteditor.AbstractTextEditor;
 
 import com.github.fabeclipse.textedgrep.GrepTool;
+import com.github.fabeclipse.textedgrep.GrepTool.GrepContext;
 
 public class GrepView extends ViewPart {
 
@@ -26,6 +40,9 @@ public class GrepView extends ViewPart {
 	private TextViewer viewer;
 	private String lastRegex;
 	private GrepTool grepTool;
+	private GrepContext grepContext;
+	private AbstractTextEditor textEd;
+	private Color highlightColor;
 
 	@Override
 	public void createPartControl(Composite parent) {
@@ -33,18 +50,18 @@ public class GrepView extends ViewPart {
 		parent.setLayout(layout);
 		layout.marginHeight = 0;
 		layout.marginWidth = 0;
-		
+
+		// add a text box for the regular expression
 		final Text regexpText = new Text(parent, SWT.SINGLE);
 		regexpText.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
 		regexpText.setText(lastRegex);
 		
+		// when pressing ENTER in the regexp field do a grep
 		regexpText.addSelectionListener(new SelectionListener() {
-			
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				// never called for Text
+				// this is never called for Text
 			}
-			
 			@Override
 			public void widgetDefaultSelected(SelectionEvent e) {
 				// the user pressed ENTER
@@ -52,12 +69,61 @@ public class GrepView extends ViewPart {
 			}
 		});
 
-		viewer = new TextViewer(parent, SWT.FLAT | SWT.V_SCROLL | SWT.H_SCROLL);
+		// vertical ruler that shows the original's line number
+		CompositeRuler ruler = new CompositeRuler();
+		ruler.addDecorator(0, new LineNumberRulerColumn() {
+			@Override
+			protected int computeNumberOfDigits() {
+				if (grepContext != null) {
+					int digits= 2;
+					double lines = grepContext.getMaxOriginalLine();
+					while (lines  > Math.pow(10, digits) -1) {
+						++digits;
+					}
+					return digits;
+				}
+				return super.computeNumberOfDigits();
+			}
+			@Override
+			protected String createDisplayString(int line) {
+				if (grepContext != null) {
+					return Integer.toString(grepContext.getOriginalLine(line) + 1);
+				}
+				return super.createDisplayString(line);
+			}
+		});
+		viewer = new SourceViewer(parent, ruler , SWT.FLAT | SWT.V_SCROLL | SWT.H_SCROLL);
 		viewer.getControl().setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		// use the default text font (the same used in text editor)
 		viewer.getTextWidget().setFont(JFaceResources.getTextFont());
-		
+		// this is a read-only view!
+		viewer.setEditable(false);
+		// highlight the cursor line
+		CursorLinePainter cursorLinePainter = new CursorLinePainter(viewer);
+		highlightColor = new Color(parent.getDisplay(), new RGB(200, 200, 0));
+		cursorLinePainter.setHighlightColor(highlightColor);
+		viewer.addPainter(cursorLinePainter);
+
 		viewer.setDocument(document);
-		
+
+		viewer.getTextWidget().addCaretListener(new CaretListener() {
+			@Override
+			public void caretMoved(CaretEvent event) {
+				int caretOffset = event.caretOffset;
+				if (grepContext != null) {
+					try {
+						int grepLine = document.getLineOfOffset(caretOffset);
+						int line = grepContext.getOriginalLine(grepLine);
+						int offset = grepContext.getDocument().getLineOffset(line);
+						textEd.selectAndReveal(offset, 0);
+					} catch (BadLocationException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+
 		IMenuManager menuManager = getViewSite().getActionBars().getMenuManager();
 		menuManager.add(new Action("Grep") {
 			@Override
@@ -71,7 +137,13 @@ public class GrepView extends ViewPart {
 	private void doGrep(final Text regexpText) {
 		lastRegex = regexpText.getText();
 		grepTool = new GrepTool(lastRegex);
-		String grep = grepTool.grepCurrentEditor(getViewSite().getWorkbenchWindow());
+		IWorkbenchWindow window = getViewSite().getWorkbenchWindow();
+		IEditorPart activeEditor = window.getActivePage().getActiveEditor();
+		if (activeEditor instanceof AbstractTextEditor) {
+			textEd = (AbstractTextEditor) activeEditor;
+		}
+		grepContext = grepTool.grepCurrentEditor(window);
+		String grep = grepContext.getText();
 		document.set(grep);
 	}
 
@@ -98,5 +170,13 @@ public class GrepView extends ViewPart {
 		if (lastRegex == null)
 			lastRegex = "";
 	}
-	
+
+	@Override
+	public void dispose() {
+		if (highlightColor != null) {
+			highlightColor.dispose();
+			highlightColor = null;
+		}
+		super.dispose();
+	}
 }
