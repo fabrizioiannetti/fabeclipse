478888d0a179569e7d073a7a2849d29cf7e360d1
Fabrizio Iannetti <fabrizio.iannetti@gmail.com>
2011-04-09 17:10:33 +0200

    small updates of feature/site description

---
+++ com.github.fabeclipse.timetrim/src/com/github/fabeclipse/timetrim/DateDropDown.java
@@ -0,0 +1,19 @@
+package com.github.fabeclipse.timetrim;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class DateDropDown extends AbstractHandler {
+
+	public DateDropDown() {
+		// TODO start a time rerfesh thread
+	}
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- com.github.fabeclipse.timetrim/src/com/github/fabeclipse/timetrim/TimeToolWorkbenchWindowControl.java
+++ com.github.fabeclipse.timetrim/src/com/github/fabeclipse/timetrim/TimeToolWorkbenchWindowControl.java
@@ -6,10 +6,12 @@ import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import org.eclipse.nebula.widgets.calendarcombo.CalendarCombo;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.DateTime;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
 import org.eclipse.ui.menus.WorkbenchWindowControlContribution;
@@ -29,8 +31,12 @@ public class TimeToolWorkbenchWindowControl extends
 	@Override
 	protected Control createControl(Composite parent) {
 //		final ToolItem item = new ToolItem((ToolBar) parent, SWT.DROP_DOWN);
-		final Button item = new Button(parent, SWT.PUSH);
-		item.setText(df.format(new Date()));
+		final Button item = new Button(parent, SWT.ARROW | SWT.FLAT);
+//		item.setText(df.format(new Date()));
+//		final DateTime item = new DateTime(parent, SWT.TIME);
+//		final DateTime item = new DateTime(parent, SWT.TIME);
+//		final CalendarCombo item = new CalendarCombo(parent, SWT.READ_ONLY | SWT.FLAT);
+//		item.setDate(new Date());
 
 		Timer timeUpdater = new Timer();
 		TimerTask task = new TimerTask() {
@@ -52,7 +58,8 @@ public class TimeToolWorkbenchWindowControl extends
 						@Override
 						public void run() {
 							if(!item.isDisposed())
-								item.setText(dateToShow);
+//								item.setText(dateToShow);
+								;
 						}
 					});
 				}
