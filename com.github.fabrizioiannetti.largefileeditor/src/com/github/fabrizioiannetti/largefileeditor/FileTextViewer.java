305c0d3f12544644362e8a5bee730f97072409c5
Fabrizio Iannetti <fabrizio.iannetti@gmail.com>
2013-08-23 16:54:46 +0200

    [largefileeditor] added text box, just to test

--- com.github.fabrizioiannetti.largefileeditor/src/com/github/fabrizioiannetti/largefileeditor/FileTextViewer.java
+++ com.github.fabrizioiannetti.largefileeditor/src/com/github/fabrizioiannetti/largefileeditor/FileTextViewer.java
@@ -2,15 +2,18 @@ package com.github.fabrizioiannetti.largefileeditor;
 
 import java.io.File;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
 
 public class FileTextViewer extends Composite {
 
-	private File textFile;
+	private FileTextModel model;
 
 	public FileTextViewer(File textFile, Composite parent, int style) {
 		super(parent, style);
-		this.textFile = textFile;
+		model = new FileTextModel(textFile, null);
+		Text text = new Text(parent, SWT.MULTI | SWT.FLAT);
+		text.setText(model.getLine(0));
 	}
-	
 }
